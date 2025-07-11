a5c30efb975a494c70064f1bb6e9d1a3ce6ab65f
mozxammil <mozxammil>
2025-07-11 15:14:17 +0530

    routing fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 const MainPage = () => {
+	const router = useRouter();
+	useEffect(() => {
+		router.push("/login");
+	}, [router]);
+
+	router.push("/login");
 	return <div></div>;
 };
 
